7bf3c3af0ad575e717f609f67f3875d6cd263b79
analog-sombra <analogsombra@gmail.com>
2025-09-30 15:36:32 +0530

    fix: upload error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["localhost", "89.116.33.19"],
+  },
 };
 
 export default nextConfig;
